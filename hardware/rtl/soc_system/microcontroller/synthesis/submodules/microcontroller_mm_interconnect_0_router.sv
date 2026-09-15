eb2bf2834d37d58844483fce533e4bbd55435345
Tharaka Jayasena <9dmpires2k17.tuj@gmail.com>
2026-09-15 23:48:04 +0530

    add sdram controller for avalon.
    still sysntax and logic errors are there.

--- hardware/rtl/soc_system/microcontroller/synthesis/submodules/microcontroller_mm_interconnect_0_router.sv
+++ hardware/rtl/soc_system/microcontroller/synthesis/submodules/microcontroller_mm_interconnect_0_router.sv
@@ -44,15 +44,15 @@
 
 module microcontroller_mm_interconnect_0_router_default_decode
   #(
-     parameter DEFAULT_CHANNEL = 3,
+     parameter DEFAULT_CHANNEL = 2,
                DEFAULT_WR_CHANNEL = -1,
                DEFAULT_RD_CHANNEL = -1,
-               DEFAULT_DESTID = 4 
+               DEFAULT_DESTID = 0 
    )
   (output [92 - 90 : 0] default_destination_id,
-   output [5-1 : 0] default_wr_channel,
-   output [5-1 : 0] default_rd_channel,
-   output [5-1 : 0] default_src_channel
+   output [6-1 : 0] default_wr_channel,
+   output [6-1 : 0] default_rd_channel,
+   output [6-1 : 0] default_src_channel
   );
 
   assign default_destination_id = 
@@ -63,7 +63,7 @@ module microcontroller_mm_interconnect_0_router_default_decode
       assign default_src_channel = '0;
     end
     else begin : default_channel_assignment
-      assign default_src_channel = 5'b1 << DEFAULT_CHANNEL;
+      assign default_src_channel = 6'b1 << DEFAULT_CHANNEL;
     end
   endgenerate
 
@@ -73,8 +73,8 @@ module microcontroller_mm_interconnect_0_router_default_decode
       assign default_rd_channel = '0;
     end
     else begin : default_rw_channel_assignment
-      assign default_wr_channel = 5'b1 << DEFAULT_WR_CHANNEL;
-      assign default_rd_channel = 5'b1 << DEFAULT_RD_CHANNEL;
+      assign default_wr_channel = 6'b1 << DEFAULT_WR_CHANNEL;
+      assign default_rd_channel = 6'b1 << DEFAULT_RD_CHANNEL;
     end
   endgenerate
 
@@ -103,7 +103,7 @@ module microcontroller_mm_interconnect_0_router
     // -------------------
     output                          src_valid,
     output reg [106-1    : 0] src_data,
-    output reg [5-1 : 0] src_channel,
+    output reg [6-1 : 0] src_channel,
     output                          src_startofpacket,
     output                          src_endofpacket,
     input                           src_ready
@@ -119,7 +119,7 @@ module microcontroller_mm_interconnect_0_router
     localparam PKT_PROTECTION_H = 96;
     localparam PKT_PROTECTION_L = 94;
     localparam ST_DATA_W = 106;
-    localparam ST_CHANNEL_W = 5;
+    localparam ST_CHANNEL_W = 6;
     localparam DECODER_TYPE = 0;
 
     localparam PKT_TRANS_WRITE = 70;
@@ -139,12 +139,13 @@ module microcontroller_mm_interconnect_0_router
     localparam PAD2 = log2ceil(64'h20040 - 64'h20000); 
     localparam PAD3 = log2ceil(64'h21008 - 64'h21000); 
     localparam PAD4 = log2ceil(64'h22800 - 64'h22000); 
+    localparam PAD5 = log2ceil(64'h2000000 - 64'h1000000); 
     // -------------------------------------------------------
     // Work out which address bits are significant based on the
     // address range of the slaves. If the required width is too
     // large or too small, we use the address field width instead.
     // -------------------------------------------------------
-    localparam ADDR_RANGE = 64'h22800;
+    localparam ADDR_RANGE = 64'h2000000;
     localparam RANGE_ADDR_WIDTH = log2ceil(ADDR_RANGE);
     localparam OPTIMIZED_ADDR_H = (RANGE_ADDR_WIDTH > PKT_ADDR_W) ||
                                   (RANGE_ADDR_WIDTH == 0) ?
@@ -168,7 +169,7 @@ module microcontroller_mm_interconnect_0_router
     assign src_startofpacket = sink_startofpacket;
     assign src_endofpacket   = sink_endofpacket;
     wire [PKT_DEST_ID_W-1:0] default_destid;
-    wire [5-1 : 0] default_src_channel;
+    wire [6-1 : 0] default_src_channel;
 
 
 
@@ -193,32 +194,38 @@ module microcontroller_mm_interconnect_0_router
         // --------------------------------------------------
 
     // ( 0x0 .. 0x10000 )
-    if ( {address[RG:PAD0],{PAD0{1'b0}}} == 18'h0   ) begin
-            src_channel = 5'b01000;
-            src_data[PKT_DEST_ID_H:PKT_DEST_ID_L] = 4;
+    if ( {address[RG:PAD0],{PAD0{1'b0}}} == 25'h0   ) begin
+            src_channel = 6'b010000;
+            src_data[PKT_DEST_ID_H:PKT_DEST_ID_L] = 5;
     end
 
     // ( 0x10000 .. 0x20000 )
-    if ( {address[RG:PAD1],{PAD1{1'b0}}} == 18'h10000   ) begin
-            src_channel = 5'b00100;
-            src_data[PKT_DEST_ID_H:PKT_DEST_ID_L] = 1;
-    end
-
-    // ( 0x20000 .. 0x20040 )
-    if ( {address[RG:PAD2],{PAD2{1'b0}}} == 18'h20000   ) begin
-            src_channel = 5'b10000;
+    if ( {address[RG:PAD1],{PAD1{1'b0}}} == 25'h10000   ) begin
+            src_channel = 6'b001000;
             src_data[PKT_DEST_ID_H:PKT_DEST_ID_L] = 2;
     end
 
-    // ( 0x21000 .. 0x21008 )
-    if ( {address[RG:PAD3],{PAD3{1'b0}}} == 18'h21000   ) begin
-            src_channel = 5'b00001;
+    // ( 0x20000 .. 0x20040 )
+    if ( {address[RG:PAD2],{PAD2{1'b0}}} == 25'h20000   ) begin
+            src_channel = 6'b100000;
             src_data[PKT_DEST_ID_H:PKT_DEST_ID_L] = 3;
     end
 
+    // ( 0x21000 .. 0x21008 )
+    if ( {address[RG:PAD3],{PAD3{1'b0}}} == 25'h21000   ) begin
+            src_channel = 6'b000001;
+            src_data[PKT_DEST_ID_H:PKT_DEST_ID_L] = 4;
+    end
+
     // ( 0x22000 .. 0x22800 )
-    if ( {address[RG:PAD4],{PAD4{1'b0}}} == 18'h22000   ) begin
-            src_channel = 5'b00010;
+    if ( {address[RG:PAD4],{PAD4{1'b0}}} == 25'h22000   ) begin
+            src_channel = 6'b000010;
+            src_data[PKT_DEST_ID_H:PKT_DEST_ID_L] = 1;
+    end
+
+    // ( 0x1000000 .. 0x2000000 )
+    if ( {address[RG:PAD5],{PAD5{1'b0}}} == 25'h1000000   ) begin
+            src_channel = 6'b000100;
             src_data[PKT_DEST_ID_H:PKT_DEST_ID_L] = 0;
     end
 
--- hardware/rtl/soc_system/microcontroller/synthesis/submodules/microcontroller_mm_interconnect_0_rsp_demux_003.sv
+++ hardware/rtl/soc_system/microcontroller/synthesis/submodules/microcontroller_mm_interconnect_0_rsp_demux_003.sv
@@ -27,11 +27,11 @@
 
 // ------------------------------------------
 // Generation parameters:
-//   output_name:         microcontroller_mm_interconnect_0_cmd_demux_001
+//   output_name:         microcontroller_mm_interconnect_0_rsp_demux_003
 //   ST_DATA_W:           106
 //   ST_CHANNEL_W:        6
 //   NUM_OUTPUTS:         2
-//   VALID_WIDTH:         6
+//   VALID_WIDTH:         1
 // ------------------------------------------
 
 //------------------------------------------
@@ -40,12 +40,12 @@
 // 15610 - Warning: Design contains x input pin(s) that do not drive logic
 //------------------------------------------
 
-module microcontroller_mm_interconnect_0_cmd_demux_001
+module microcontroller_mm_interconnect_0_rsp_demux_003
 (
     // -------------------
     // Sink
     // -------------------
-    input  [6-1      : 0]   sink_valid,
+    input  [1-1      : 0]   sink_valid,
     input  [106-1    : 0]   sink_data, // ST_DATA_W=106
     input  [6-1 : 0]   sink_channel, // ST_CHANNEL_W=6
     input                         sink_startofpacket,
@@ -92,14 +92,14 @@ module microcontroller_mm_interconnect_0_cmd_demux_001
         src0_endofpacket   = sink_endofpacket;
         src0_channel       = sink_channel >> NUM_OUTPUTS;
 
-        src0_valid         = sink_channel[0] && sink_valid[0];
+        src0_valid         = sink_channel[0] && sink_valid;
 
         src1_data          = sink_data;
         src1_startofpacket = sink_startofpacket;
         src1_endofpacket   = sink_endofpacket;
         src1_channel       = sink_channel >> NUM_OUTPUTS;
 
-        src1_valid         = sink_channel[1] && sink_valid[1];
+        src1_valid         = sink_channel[1] && sink_valid;
 
     end
 
